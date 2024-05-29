df1ebd78baef86f60552b104a64c5189b7bfae34
LosLordos <126649677+LosLordos@users.noreply.github.com>
2024-05-29 23:17:24 +0200

    Java třídy finální úpravy

---
+++ calendaar/CalendarAdapter.java
@@ -0,0 +1,69 @@
+package com.example.calendaar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder> {
+    private ArrayList<String> daysOfMonth;
+    private OnItemListener onItemListener;
+
+    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener) {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
+    public CalendarAdapter(ArrayList<LocalDate> days, WeekViewActivity onItemListener) {
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.calendar_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalendarViewHolder(view, onItemListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+        holder.dayOfMonth.setText(daysOfMonth.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysOfMonth.size();
+    }
+
+    public interface OnItemListener {
+        void onItemClick(int position, String dayText);
+
+        void onItemClick(int position, LocalDate date);
+    }
+
+    static class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final TextView dayOfMonth;
+        private final OnItemListener onItemListener;
+
+        public CalendarViewHolder(@NonNull View itemView, OnItemListener onItemListener) {
+            super(itemView);
+            dayOfMonth = itemView.findViewById(R.id.cellDayText);
+            this.onItemListener = onItemListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+        }
+    }
+}
+
---
+++ calendaar/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.calendaar;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            case 2:
+                return new ThirdFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+    public class FirstFragment extends Fragment {
+        // Obsah první stránky
+    }
+
+    public class SecondFragment extends Fragment {
+        // Obsah druhé stránky
+    }
+
+    public class ThirdFragment extends Fragment {
+        // Obsah třetí stránky
+    }
+
+
+}
